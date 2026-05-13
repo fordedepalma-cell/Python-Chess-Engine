3eea3d8c7fce3374067da86b4fc2a734f20b5921
fordedepalma-cell <fordedepalma@gmail.com>
2026-05-13 14:50:46 -0500

    Another Commit

--- ai_testing.py
+++ ai_testing.py
@@ -9,30 +9,42 @@ import os
 load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-with OpenRouter(api_key=OPENROUTER_API_KEY) as client:
-    response = client.chat.send(
-        model="openai/gpt-5.2",
-        messages=[
-            {"role": "user", "content": "Hello there!"}
-        ],
-    )
-    print(response.choices[0].message.content)
+print(OPENROUTER_API_KEY, GROQ_API_KEY)
 
-response = requests.post(
-    url="https://openrouter.ai/api/v1/chat/completions",
-    headers={
-        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-    },
-    data=json.dumps({
-        "model": "openai/gpt-5.2",
-        "messages": [
-            {
-                "role": "user",
-                "content": "Hello there!"
-            }
-        ]
-    })
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-print(response.json())
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
+
+# response = requests.post(
+#     url="https://openrouter.ai/api/v1/chat/completions",
+#     headers={
+#         "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+#     },
+#     data=json.dumps({
+#         "model": "openai/gpt-5.2",
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": "Hello there!"
+#             }
+#         ]
+#     })
+# )
+#
+# print(response.json())
